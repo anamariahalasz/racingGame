c00b07cf823953d5dd6b44fcd13dd184e2804328
anamariahalasz <ana.mariahalasz@yahoo.com>
2018-12-19 21:35:54 +0200

    studied encapsulation

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -8,32 +8,48 @@ public class App
 {
     public static void main( String[] args )
     {
-        // declaring a variable and instalating a Car object
-        // this is also called initializing a variable
-       Car firstCompetitor = new Car(new Engine());
-       firstCompetitor.name = "Dacia";
-       firstCompetitor.color = "black";
-       firstCompetitor.doorCount = 5;
-       firstCompetitor.mileage = 9.5;
+//        Engine firstEngine = new Engine();
+//        firstEngine.manufacturer = "Renault";
+//
+//        // declaring a variable and instalating a Car object
+//        // this is also called initializing a variable
+//       Car firstCompetitor = new Car(firstEngine);
+//       firstCompetitor.setName("Dacia");
+//       firstCompetitor.setColor("black");
+//       firstCompetitor.doorCount = 5;
+//       firstCompetitor.setMileage(9.5);
+//
+//        // local variables are variables declared inside a method
+//        // they don`t receive default values
+//        Car secondCompetitor = new Car(new Engine());
+//        secondCompetitor.setName("Golf");
+//
+//        // sout prints a message to the console
+//        System.out.println(firstEngine.manufacturer);
+//
+//
+//
+//       firstCompetitor.engine = firstEngine;
+//       firstCompetitor.engine.manufacturer = "BMW";
+//
+//        System.out.println(firstEngine.manufacturer);
+//        System.out.println(firstCompetitor.engine.manufacturer);
+//
+//        double traveledDistance = firstCompetitor.accelerate(100, 30);
+//
+        Vehicle vehicle = new Vehicle();
+        vehicle.applicationTotalNumberOfVehicles = 1;
 
-        // local variables are variables declared inside a method
-        // they don`t receive default values
-        Car secondCompetitor = new Car(new Engine());
-        secondCompetitor.name = "Golf";
+        System.out.println("Total from vehicle 1: " + vehicle.applicationTotalNumberOfVehicles);
 
-       Engine firstEngine = new Engine();
-       firstEngine.manufacturer = "Renault";
+        Vehicle vehicle2 = new Vehicle();
+        vehicle2.applicationTotalNumberOfVehicles = 2;
 
-       // sout prints a message to the console
-        System.out.println(firstEngine.manufacturer);
+        System.out.println("Total from vehicle 2: " + vehicle2.applicationTotalNumberOfVehicles);
+        System.out.println("Total from vehicle 1: " + vehicle.applicationTotalNumberOfVehicles);
 
-       firstCompetitor.engine = firstEngine;
-       firstCompetitor.engine.manufacturer = "BMW";
 
-        System.out.println(firstEngine.manufacturer);
-        System.out.println(firstCompetitor.engine.manufacturer);
 
-        double traveledDistance = firstCompetitor.accelerate(100, 30);
 
 
     }
--- src/main/java/org/fasttrackit/Vehicle.java
+++ src/main/java/org/fasttrackit/Vehicle.java
@@ -2,10 +2,14 @@ package org.fasttrackit;
 
 public class Vehicle {
 
-    String name;
-    String color;
-    double mileage;
-    long totalKm;
+    public static int applicationTotalNumberOfVehicles;
+
+   private String name;
+   private String color;
+   private double mileage;
+   private long totalKm;
+
+
 
     public double accelerate (double speed, double durationInHours) {
         System.out.println(name + " accelerated with " + speed + " km/h for " + durationInHours + " hours. ");
@@ -16,4 +20,35 @@ public class Vehicle {
 
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name.trim();
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(double mileage) {
+        this.mileage = mileage;
+    }
+
+    public long getTotalKm() {
+        return totalKm;
+    }
+
+    public void setTotalKm(long totalKm) {
+        this.totalKm = totalKm;
+    }
 }
